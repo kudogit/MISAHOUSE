e7266af07d87701d80e5c2f6f23c34416ab7c019
KhoiSTS <khoiaesgit@gmail.com>
2018-07-08 14:56:40 +0700

    abc

---
+++ Misa.Web/wwwroot/admin.module.chunk.js
@@ -0,0 +1,41 @@
+webpackJsonp(["admin.module"],{
+
+/***/ "./src/app/modules/themes/admin.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AdminModule", function() { return AdminModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__admin_routing_module__ = __webpack_require__("./src/app/modules/themes/admin-routing.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__layout_layout_module__ = __webpack_require__("./src/app/modules/layout/layout.module.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AdminModule = /** @class */ (function () {
+    function AdminModule() {
+    }
+    AdminModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["I" /* NgModule */])({
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1__admin_routing_module__["a" /* AdminRoutingModule */],
+                __WEBPACK_IMPORTED_MODULE_2__layout_layout_module__["a" /* LayoutModule */]
+            ],
+            exports: []
+        })
+    ], AdminModule);
+    return AdminModule;
+}());
+
+
+
+/***/ })
+
+});
+//# sourceMappingURL=admin.module.chunk.js.map
